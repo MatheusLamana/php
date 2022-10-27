cabbb66003515cf53f0454fc89b1574ec7bdaaf9
Matheus <67989031+MatheusLamana@users.noreply.github.com>
2022-10-27 13:51:55 -0300

    Add files via upload

---
+++ testes/meses.php
@@ -0,0 +1,47 @@
+<?php
+
+$meses = array(
+    1 => "Janeiro",
+         "Fevereiro",
+         "Março",
+         "Abril",
+         "Maio",
+         "Junho",
+         "Julho",
+         "Agosto",
+         "Setembro",
+         "Outubro",
+         "Novembro",
+         "Dezembro",
+);
+
+$valor = $_POST['aaa'];
+
+if(!str_starts_with($valor, 0)){
+    if($valor > 12){
+        echo "Valor não pode ser maior que 12";
+        exit;
+    }
+    echo "$meses[$valor]";
+} else {
+    echo "Pesquise o mês sem o zero";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" name="aaa">
+        <input type="submit" name="enviar" value="Enviar!">
+    </form>
+</body>
+</html>
